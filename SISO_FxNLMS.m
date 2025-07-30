5c96d0cad624bbe665a66f14fb154ecec533ec8d
Philip <philip_zhong@126.com>
2025-07-30 10:57:33 +0800

    if there no mu_in input, then use default value

--- SISO_FxNLMS.m
+++ SISO_FxNLMS.m
@@ -8,7 +8,15 @@ function [y, W_out] = SISO_FxNLMS(u, e, mu_in)
         X = zeros(L, 1);
         eps = 1e-6;
         alpha = 1e-4;
-        mu = mu_in;  % 将传入的 mu 保存在内部
+
+        % 如果没有传入 mu_in，使用默认值
+        if nargin < 3
+            mu = 0.02;  % 默认步长值（你可以根据需要调整）
+        else
+            mu = mu_in;  % 使用传入的 mu 值
+        end
+
+        % 加载次级路径估计
         load('2ndPath.mat', 'w');
         if isrow(w), w = w'; end
         if length(w) ~= L
@@ -17,19 +25,20 @@ function [y, W_out] = SISO_FxNLMS(u, e, mu_in)
         S_hat = w;
     end
 
-    % 输入缓冲
+    % 输入缓冲区更新
     X = [u; X(1:end-1)];
 
     % Filtered-x
     x_f = conv(X, S_hat, 'same');
 
-    % 输出
+    % 输出计算
     y = W' * x_f;
 
     % 权重更新
     norm_factor = x_f' * x_f + eps;
     W = (1 - alpha) * W + mu * x_f * e / norm_factor;
 
+    % 返回权重
     if nargout > 1
         W_out = W;
     end
